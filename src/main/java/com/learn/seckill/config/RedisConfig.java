b9e4a9f37a664889684598cdc1bb05b420112917
chaofan.zhang <chaofan.zhang@zhangmen.com>
2019-11-29 14:07:39 +0800

    集成redis

---
+++ src/main/java/com/learn/seckill/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.learn.seckill.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @program: seckill:RedisConfig 配置工具类
+ * @description: 注入工具类
+ * @author: zcf
+ * @create: 2019-11-21 19:43
+ **/
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(factory);
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        // 设置键（key）的序列化采用StringRedisSerializer。
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // 设置值（value）的序列化采用FastJsonRedisSerializer。
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ src/test/java/com/learn/seckill/RedisTest.java
@@ -0,0 +1,29 @@
+package com.learn.seckill;
+
+import com.learn.seckill.utils.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @program: seckill:RedisTest
+ * @description:
+ * @author: zcf
+ * @create: 2019-11-29 13:58
+ **/
+@Slf4j
+public class RedisTest extends BaseTest {
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void testSet() {
+        boolean flag = redisUtil.set("test", "test");
+        log.info("flag:{}", flag);
+    }
+    @Test
+    public void testGet() {
+        String value = redisUtil.get("test").toString();
+        log.info("value:{}", value);
+    }
+}
